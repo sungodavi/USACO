025fbd5868aab1aba78b254f14af32e7cd62c201
Bob Wilson <sungodavi@gmail.com>
2018-03-04 11:58:34 -0600

    asdf

--- training/src/milk4.java
+++ training/src/milk4.java
@@ -7,15 +7,16 @@ TASK: milk4
 import java.util.*;
 import java.io.*;
 import java.awt.Point;
-import java.math.BigInteger;
 
 class milk4 
 {
-	static BigInteger[][] dp;
-	static boolean[][] visited;
+	static int[][] dp, first;
+	static Point[][] next;
 	static int[] a;
+	static int INF = 101;
 	public static void main(String[] args) throws IOException 
 	{
+		long start = System.currentTimeMillis();
 		BufferedReader f = new BufferedReader(new FileReader("milk4.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("milk4.out")));
 		int num = Integer.parseInt(f.readLine());
@@ -23,56 +24,66 @@ class milk4
 		a = new int[size];
 		for(int i = 1; i < size; i++)
 			a[i] = Integer.parseInt(f.readLine());
+		Arrays.sort(a);
 //		System.out.println(Arrays.toString(a));
-		dp = new BigInteger[num + 1][size];
-		visited = new boolean[num + 1][size];
-		BigInteger result = dfs(num, 0);
-		display(result, out);
+		dp = new int[num + 1][size];
+		first = new int[num + 1][size];
+		next = new Point[num + 1][size];
+		for(int[] temp : dp)
+			Arrays.fill(temp, -1);
+		first = new int[num + 1][size];
+		out.print(dfs(num, 0));
+		display(out);
+		System.out.println(System.currentTimeMillis() - start);
 		out.close();
 	}
 	
-	public static BigInteger dfs(int u, int curr)
+	public static void display(PrintWriter out)
+	{
+		int coin = 0;
+		int u = dp.length - 1;
+		int curr = 0;
+		while(u > 0)
+		{
+			if(curr != coin)
+				out.print(" " + a[coin = curr]);
+			Point p = next[u][curr];
+			u = p.x;
+			curr = p.y;
+		}
+		if(coin != curr)
+			out.print(" " + a[curr]);
+		out.println();
+	}
+	
+	public static int dfs(int u, int curr)
 	{
 		if(u == 0)
-			return BigInteger.ZERO;
-		if(visited[u][curr])
+			return 0;
+		if(dp[u][curr] != -1)
 			return dp[u][curr];
-
-		BigInteger result = null;
+		int result = INF;
+		Point best = null;
 		for(int i = Math.max(1, curr); i < a.length; i++)
 		{
 			if(a[i] > u)
 				break;
-			BigInteger p = dfs(u - a[i], i);
-			if(p != null)
+			int v = u - a[i];
+			int p = dfs(v, i);
+			if(i != curr)
 			{
-				if(curr != i)
-					p = p.setBit(i);
-				if(result == null || comp(p, result))
-					result = p;
+				p++;
+				first[v][i] = i;
+			}
+			if(p < 101 && (best == null || p < result || p == result && first[v][i] < first[best.x][best.y]))
+			{
+				best = new Point(v, i);
+				result = p;
+				first[u][curr] = i;
 			}
 		}
-//		System.out.println(u + " " + curr + (result == null ? "" : " " + result.toString(2)));
-		visited[u][curr] = true;
+//		System.out.println(u + " " + curr + " " + result + " " + first[u][curr] + " " + best);
+		next[u][curr] = best;
 		return dp[u][curr] = result;
-	}
-	
-	public static void display(BigInteger result, PrintWriter out)
-	{
-		out.print(result.bitCount());
-		while(result.compareTo(BigInteger.ZERO) > 0)
-		{
-			int p = result.getLowestSetBit();
-			out.print(" " + a[p]);
-			result = result.clearBit(p);
-		}
-		out.println();
-	}
-	
-	public static boolean comp(BigInteger p, BigInteger curr)
-	{
-		int c1 = p.bitCount();
-		int c2 = curr.bitCount();
-		return c1 < c2 || (c1 == c2 && p.getLowestSetBit() < curr.getLowestSetBit());
 	}
 }
